6a05654986ffb9c9168bbc486deae274907bf449
Lucas Jeon <wookee329@gmail.com>
2022-01-12 17:59:11 +0900

    react and web audio api study start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Body = styled.div`
+  display: flex;
+`;
+const NavigationBarContainer = styled.div`
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  height: 60px;
+  background-color: #121212;
+`;
+
+const MenuGroupStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Menu = ({ text }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignContent: "center",
+        width: "60px",
+        height: "60px",
+        marginLeft: "20px",
+        color: "white",
+      }}
+    >
+      {text}
+    </div>
+  );
+};
+
+const Logo = () => {
+  return (
+    <div
+      style={{
+        width: "100px",
+        height: "60px",
+        backgroundImage: `url(http://localhost:3000/public/splice.png)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    />
+  );
+};
+
+const NavigationBar = () => {
+  return (
+    <NavigationBarContainer>
+      <MenuGroupStyle>
+        <Logo />
+        <Menu text={"Sounds"} />
+        <Menu text={"Skills"} />
+        <Menu text={"Plugins"} />
+        <Menu text={"Studio"} />
+        <Menu text={"Community"} />
+        <Menu text={"Blog"} />
+      </MenuGroupStyle>
+      <MenuGroupStyle style={{ marginLeft: "auto" }}>
+        <Menu text={"Invite Friends"} />
+        <Menu text={"Help"} />
+        <Menu text={"Volume"} />
+        <Menu text={"Profile"} />
+      </MenuGroupStyle>
+    </NavigationBarContainer>
+  );
+};
+
+const SearchBarContainer = styled.div`
+  display: flex;
+`;
+const SearchBar = styled.div`
+  height: 49px;
+  background-color: green;
+`;
+
+const Main = styled.div`
+  display: flex;
+`;
+
+const SideBar = styled.div`
+  width: 203px;
+  height: 900px;
+  background-color: yellow;
+`;
+
+const Contents = styled.div`
+  width: 1000px;
+  height: 900px;
+  background-color: blue;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar />
+      <SearchBar />
+      <Main>
+        <SideBar />
+        <Contents />
+      </Main>
     </div>
   );
 }
